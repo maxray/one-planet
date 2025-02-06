0f445b2f76bdbb1e2b479d06236a72fdf0773953
Sush <sushkelly@gmail.com>
2025-02-06 16:24:50 +0000

    FIX: checking release tags

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: One Planet Living
  * Plugin URI: https://github.com/maxray/one-planet
  * Description: An interactive tool to help you create your own One Planet Living Action Plan.
- * Version: 1.2
+ * Version: 1.3
  * Author: Sush Kelly
  * Author URI: https://sushkelly.co.uk
  * License: GPL2
